e2ddbcb36868c37e494f97caf2b10241e334d999
Paula Segatelli <pc.segatelli@gmail.com>
2023-12-24 16:23:45 -0300

    add Modelo Aluno

--- MinhaAppFuncional/src/AppMvcFuncional/Controllers/HomeController.cs
+++ MinhaAppFuncional/src/AppMvcFuncional/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace AppMvcFuncional.Controllers
 
         public IActionResult Index()
         {
+            Console.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT"));
             return View();
         }
 
